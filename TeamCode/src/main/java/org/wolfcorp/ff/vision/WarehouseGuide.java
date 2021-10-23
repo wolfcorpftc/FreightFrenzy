a48103e8e090632e026f514a617e807095dd701e
Zhenkai Weng <theoaktree1040@gmail.com>
2021-10-23 02:09:08 -0700

    Use lock instead of volatile in WarehouseGuide

--- TeamCode/src/main/java/org/wolfcorp/ff/vision/WarehouseGuide.java
+++ TeamCode/src/main/java/org/wolfcorp/ff/vision/WarehouseGuide.java
@@ -17,12 +17,14 @@ import java.util.concurrent.CountDownLatch;
 public class WarehouseGuide extends Detector {
     private static final double INVALID_ANGLE = Double.MIN_VALUE;
 
-    private Mat mat = null;
-    private Mat hierarchy = null;
+    private Mat mat = new Mat();
+    private Mat hierarchy = new Mat();
     private ArrayList<MatOfPoint> contours = new ArrayList<>();
-    private volatile Freight target = Freight.GOLD;
-    private volatile double targetAngle = INVALID_ANGLE;
-    private ResettableCountDownLatch latch = new ResettableCountDownLatch(1);
+    private Freight target = Freight.GOLD;
+    private double targetAngle = INVALID_ANGLE;
+
+    private final Object updateLock = new Object();
+    private final ResettableCountDownLatch latch = new ResettableCountDownLatch(1);
 
     public WarehouseGuide(OpenCvWebcam cam) {
         super(cam);
@@ -34,78 +36,82 @@ public class WarehouseGuide extends Detector {
     }
 
     public Mat processFrame(Mat input) {
-        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGBA2RGB);
-        // Use blurring to reduce extraneous contours (e.g., holes in silver)
-        Imgproc.medianBlur(mat, mat, 20);
-        // TODO: Idea -- perform edge detection on mat here, bitwise-and that matrix with threshed,
-        //   and add to the contour matrix; erode, edge-detect, and dilate if necessary
-        if (target == Freight.GOLD) {
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2HSV);
-            Scalar lowerBound = new Scalar(0, 198, 0);
-            Scalar upperBound = new Scalar(180, 255, 255);
-            Core.inRange(mat, lowerBound, upperBound, mat);
-        }
-        else { // target == Freight.SILVER
-            // HSL is better for identifying the color white
-            Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2HLS);
-            // TODO: color temperature? normalize on field, make it tunable (text file?)
-            Scalar lowerBound = new Scalar(0, 216.75, 0);
-            Scalar upperBound = new Scalar(179, 255, 255);
-            Core.inRange(mat, lowerBound, upperBound, mat);
-        }
-        Imgproc.erode(mat, mat, new Mat(), new Point(-1, -1), 15);
-        Imgproc.dilate(mat, mat, new Mat(), new Point(-1, -1), 15);
-        targetAngle = findFreightAngle(input);
-        latch.countDown();
-        return input;
-    }
+        synchronized (updateLock) {
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGBA2RGB);
+            // Use blurring to reduce extraneous contours (e.g., holes in silver)
+            Imgproc.medianBlur(mat, mat, 20);
+            // TODO: Idea -- perform edge detection on mat here, bitwise-and that matrix with threshed,
+            //   and add to the contour matrix; erode, edge-detect, and dilate if necessary
 
-    // Calculate angle to freight given a threshed image (mat)
-    protected double findFreightAngle(Mat input) {
-        // *** Find coutours ***
-        Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-        double width = mat.width();
-        double height = mat.height();
-        mat.release();
-        hierarchy.release();
-
-        // *** Find bounding rectangles ***
-        MatOfPoint2f contoursPoly = new MatOfPoint2f();
-        Rect[] boundRect = new Rect[contours.size()];
-        Scalar rectColor =
-                target == Freight.GOLD ? new Scalar(255, 171, 23, 1) : new Scalar(192, 192, 192, 1);
-        for (int i = 0; i < contours.size(); i++) {
-            Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly, 3, true);
-            boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly.toArray()));
-            contours.get(i).release();
-            contoursPoly.release();
-            Imgproc.rectangle(input, boundRect[i], rectColor);
-        }
-
-        // *** Find center coordinates of rectangles ***
-        Point[] centers = new Point[contours.size()];
-        for (int i = 0; i < contours.size(); i++) {
-            centers[i] = new Point(
-                    boundRect[i].x + boundRect[i].width / 2.0,
-                    boundRect[i].y + boundRect[i].height / 2.0
-            );
-        }
-
-        // *** Find coordinates closest to robot ***
-        Point minPoint = new Point();
-        double minDist = Double.MAX_VALUE;
-        Point robot = new Point(width / 2.0, height);
-        for (Point c : centers) {
-            double dist = Math.hypot(c.x - robot.x, c.y - robot.y);
-            if (dist < minDist) {
-                minPoint = c;
-                minDist = dist;
+            // *** Thresholding ***
+            if (target == Freight.GOLD) {
+                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2HSV);
+                Scalar lowerBound = new Scalar(0, 198, 0);
+                Scalar upperBound = new Scalar(180, 255, 255);
+                Core.inRange(mat, lowerBound, upperBound, mat);
+            } else { // target == Freight.SILVER
+                // HSL is better for identifying the color white
+                Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2HLS);
+                // TODO: color temperature? normalize on field, make it tunable (text file?)
+                Scalar lowerBound = new Scalar(0, 216.75, 0);
+                Scalar upperBound = new Scalar(179, 255, 255);
+                Core.inRange(mat, lowerBound, upperBound, mat);
             }
-        }
-        Imgproc.circle(input, minPoint, 15, new Scalar(0, 255, 0, 1), 5);
 
-        // assume a 60 deg field of view (value sourced from cam spec)
-        return 60 * minPoint.x / width - 30.0;
+            // *** Separate overlapping blobs ***
+            Imgproc.erode(mat, mat, new Mat(), new Point(-1, -1), 15);
+            Imgproc.dilate(mat, mat, new Mat(), new Point(-1, -1), 15);
+
+            // *** Find contours ***
+            Imgproc.findContours(mat, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+            double width = mat.width();
+            double height = mat.height();
+            mat.release();
+            hierarchy.release();
+
+            // *** Find bounding rectangles ***
+            MatOfPoint2f contoursPoly = new MatOfPoint2f();
+            Rect[] boundRect = new Rect[contours.size()];
+            Scalar rectColor =
+                    target == Freight.GOLD ? new Scalar(255, 171, 23, 1) : new Scalar(192, 192, 192, 1);
+            for (int i = 0; i < contours.size(); i++) {
+                Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(i).toArray()), contoursPoly, 3, true);
+                boundRect[i] = Imgproc.boundingRect(new MatOfPoint(contoursPoly.toArray()));
+                contours.get(i).release();
+                contoursPoly.release();
+                Imgproc.rectangle(input, boundRect[i], rectColor);
+            }
+            contours.clear();
+
+            // *** Find center coordinates of rectangles ***
+            Point[] centers = new Point[boundRect.length];
+            for (int i = 0; i < boundRect.length; i++) {
+                centers[i] = new Point(
+                        boundRect[i].x + boundRect[i].width / 2.0,
+                        boundRect[i].y + boundRect[i].height / 2.0
+                );
+            }
+
+            // *** Find coordinates closest to robot ***
+            Point minPoint = new Point();
+            double minDist = Double.MAX_VALUE;
+            // TODO: change the point depending on camera placement
+            Point robot = new Point(width / 2.0, height);
+            for (Point c : centers) {
+                double dist = Math.hypot(c.x - robot.x, c.y - robot.y);
+                if (dist < minDist) {
+                    minPoint = c;
+                    minDist = dist;
+                }
+            }
+            Imgproc.circle(input, minPoint, 15, new Scalar(0, 255, 0, 1), 5);
+
+            // assume a 60 deg field of view (value sourced from cam spec)
+            targetAngle = 60 * minPoint.x / width - 30.0;
+            latch.countDown();
+        }
+
+        return input;
     }
 
     public double getTargetAngle() throws InterruptedException {
@@ -120,13 +126,10 @@ public class WarehouseGuide extends Detector {
     }
 
     public void setTargetType(Freight f) {
-        if (f != target) {
+        synchronized (updateLock) {
             latch.reset();
             target = f;
             targetAngle = INVALID_ANGLE;
-        }
-        else {
-            target = f;
         }
     }
 }
