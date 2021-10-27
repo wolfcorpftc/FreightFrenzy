94e7ed813460d79b555e35e16d9b45f5fde94be2
Zhenkai Weng <theoaktree1040@gmail.com>
2021-10-27 15:49:48 -0700

    Fix TeleOpMode indentation

--- TeamCode/src/main/java/org/wolfcorp/ff/opmode/TeleOpMode.java
+++ TeamCode/src/main/java/org/wolfcorp/ff/opmode/TeleOpMode.java
@@ -12,32 +12,33 @@ import org.wolfcorp.ff.robot.Drivetrain;
 public class TeleOpMode extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-            FtcDashboard dashboard = FtcDashboard.getInstance();
-            Drivetrain drive = new Drivetrain(hardwareMap);
-            ElapsedTime timer = new ElapsedTime();
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        Drivetrain drive = new Drivetrain(hardwareMap);
+        ElapsedTime timer = new ElapsedTime();
 
-            // Send telemetry message to signify robot waiting;
-            telemetry.addData("Message", "Hello Driver");
-            telemetry.update();
-            // Wait for the game to start (driver presses PLAY)
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Message", "Hello Driver");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
 
-            Pose2d initialPose = new Pose2d(12, 36, Math.toRadians(0));
+        Pose2d initialPose = new Pose2d(12, 36, Math.toRadians(0));
 
-            drive.setPoseEstimate(initialPose);
+        drive.setPoseEstimate(initialPose);
 
-            waitForStart();
+        waitForStart();
 
-            timer.reset();
-            while (opModeIsActive()) {
-                // Drivetrain
-                drive.drive(
-                        Math.abs(gamepad1.right_stick_y) < 0.8 && Math.abs(gamepad1.right_stick_y) > 0.05 ? gamepad1.right_stick_y > 0 ? -0.5 : 0.5 : -gamepad1.right_stick_y,
-                        Math.abs(gamepad1.right_stick_x) < 0.8 && Math.abs(gamepad1.right_stick_x) > 0.05 ? gamepad1.right_stick_x > 0 ?  0.5 : -0.5 : gamepad1.right_stick_x,
-                        Math.abs(gamepad1.left_stick_x) < 0.6  && Math.abs(gamepad1.left_stick_x) > 0.05  ? gamepad1.left_stick_x > 0  ?  0.3 : -0.3 : gamepad1.left_stick_x,
-                        0.4,
-                        gamepad1.right_trigger > 0.8);
-                drive.update(); // odometry update
-            }
+        timer.reset();
+        while (opModeIsActive()) {
+            // Drivetrain
+            drive.drive(
+                    Math.abs(gamepad1.right_stick_y) < 0.8 && Math.abs(gamepad1.right_stick_y) > 0.05 ? gamepad1.right_stick_y > 0 ? -0.5 : 0.5 : -gamepad1.right_stick_y,
+                    Math.abs(gamepad1.right_stick_x) < 0.8 && Math.abs(gamepad1.right_stick_x) > 0.05 ? gamepad1.right_stick_x > 0 ?  0.5 : -0.5 : gamepad1.right_stick_x,
+                    Math.abs(gamepad1.left_stick_x) < 0.6  && Math.abs(gamepad1.left_stick_x) > 0.05  ? gamepad1.left_stick_x > 0  ?  0.3 : -0.3 : gamepad1.left_stick_x,
+                    0.4,
+                    gamepad1.right_trigger > 0.8
+            );
+            drive.update(); // odometry update
+        }
 
     }
 }
