6f391b77090e43eac482d74014fa16698d87f9f5
Zhenkai Weng <theoaktree1040@gmail.com>
2021-11-17 23:44:57 -0800

    Match class pedantry

--- TeamCode/src/main/java/org/wolfcorp/ff/opmode/Match.java
+++ TeamCode/src/main/java/org/wolfcorp/ff/opmode/Match.java
@@ -5,11 +5,13 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import org.wolfcorp.ff.robot.DriveConstants;
 
 /**
- * Simple static field serving as a storage medium for the bot's pose.
+ * Storage for states that should persist during the duration of the match.
  * This allows different classes/opmodes to set and read from a central source of truth.
- * A static field allows data to persist between opmodes.
  */
-public class Match {
+public final class Match {
+    // Disallow instantiation
+    private Match() {}
+
     // Temporary values to prevent errors if Tele-op is run without Autonomous
     // Will be replaced with actual locations when autonomous runs
     public static Pose2d teleOpInitialPose = new Pose2d(12, 72 - DriveConstants.WIDTH / 2, Math.toRadians(180));
