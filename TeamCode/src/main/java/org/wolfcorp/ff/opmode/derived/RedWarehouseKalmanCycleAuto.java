d246609b63e054722fc50c6d0fcf431ad12ddec2
Joshua <joshuahsu108@gmail.com>
2022-04-08 20:09:24 -0700

    Set up kalman auto

---
+++ TeamCode/src/main/java/org/wolfcorp/ff/opmode/derived/RedWarehouseKalmanCycleAuto.java
@@ -0,0 +1,8 @@
+package org.wolfcorp.ff.opmode.derived;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.wolfcorp.ff.opmode.KalmanAutonomousMode;
+
+@Autonomous(name = "Kalman Red Warehouse Cycle", group = "!main")
+public class RedWarehouseKalmanCycleAuto extends KalmanAutonomousMode {}
